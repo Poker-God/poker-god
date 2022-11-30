de75d9660d459d4947c23e3ba2b245db7026328f
Yutari11 <117772209+Yutari11@users.noreply.github.com>
2022-11-30 14:33:58 +0100

    Add files via upload

--- locateCards.py
+++ locateCards.py
@@ -26,7 +26,8 @@ def PIL_to_cv2(img):
 
 
 #Get the screenshot, and crop it to only include the cards (5 max)
-screen = Image.open(r"C:\Users\vlebo\Desktop\Screen Poker\screen_cards.jpeg")
+screen = Image.open(r"C:\Users\vlebo\Documents\SEMESTRE 7\Software Engineering\Screen Poker\screen_cards.jpeg")
+#screen_crop = screen
 screen_crop = screen.crop((500,420, 880, 520))
 #screen_crop.show()
 
@@ -39,14 +40,14 @@ opencv_image = PIL_to_cv2(screen_crop)
 gray_img = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
 #blur_img = cv2.GaussianBlur(gray_img,(5,5),0)
 thresh_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-cv2.imshow('Gris', thresh_img)
-cv2.waitKey()
+#cv2.imshow('Gris', thresh_img)
+#cv2.waitKey()
 
 
 #Get the contours of the image, draw them on the image to show they are correct
 allcontours = cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = allcontours[0]
-cv2.drawContours(opencv_image, contours, -1, (0,255,0), 3)
+cv2.drawContours(opencv_image, contours, -1, (255,255,0), 3)
 
 
 #Crop the original cropped image to get each card individually in an image file
@@ -62,23 +63,63 @@ for i in contours:
     im_temp = screen_crop.crop((minleft,minbottom, maxleft, maxbottom))
     im_cartes.append(im_temp)
 
-for i in im_cartes:
-    plt.imshow(i)
+for i in range(len(im_cartes)):
+    im_cartes[i] = cv2_to_PIL(cv2.resize(PIL_to_cv2(im_cartes[i]), (76,100), interpolation = cv2.INTER_AREA))
+    print(im_cartes[i].size)
+    plt.imshow(im_cartes[i])
     plt.show()
 
 cv2.imwrite("carte.png", PIL_to_cv2(im_cartes[0]))
-spade = cv2.imread(r"C:\Users\vlebo\Desktop\Symbole\spade.jpeg")
-diamond = cv2.imread(r"C:\Users\vlebo\Desktop\Symbole\diamond.jpeg")
-club = cv2.imread(r"C:\Users\vlebo\Desktop\Symbole\club.jpeg")
-heart = cv2.imread(r"C:\Users\vlebo\Desktop\Symbole\heart.jpeg")
+spade = cv2.imread(r"C:\Users\vlebo\Documents\SEMESTRE 7\Software Engineering\Symboles 2\spade.png")
+diamond = cv2.imread(r"C:\Users\vlebo\Documents\SEMESTRE 7\Software Engineering\Symboles 2\diamond.png")
+club = cv2.imread(r"C:\Users\vlebo\Documents\SEMESTRE 7\Software Engineering\Symboles 2\club.png")
+heart = cv2.imread(r"C:\Users\vlebo\Documents\SEMESTRE 7\Software Engineering\Symboles 2\heart.png")
 
 symbols = [spade, diamond, club, heart]
 
-for s in symbols:
-    res = cv2.matchTemplate(PIL_to_cv2(im_cartes[0]), s, cv2.TM_SQDIFF_NORMED)
-    threshold = .1
-    loc = numpy.where(res >= threshold)
-    print(loc)
+
+def differentiate_red_black(PILimg):
+    px = PILimg.load()
+    color = ""
+    if px[13,44][0] > 200 and px[13,44][1] < 100 and px[13,44][2] < 100:
+        color = "red"
+    elif px[13,44][0] <10 and px[13,44][1] < 10 and px[13,44][2] < 10:
+        color = "black"
+    else:
+        color = "unknown"
+    return color
+
+def differentiate_red(PILimg):
+    px = PILimg.load()
+    color = ""
+    if px[7,36][0] > 200 and px[7,36][1] < 100 and px[7,36][2] < 100:
+        color = "heart"
+    else:
+        color = "diamond"
+    return color
+
+def differentiate_black(PILimg):
+    px = PILimg.load()
+    color = ""
+    if px[8, 41][0] < 50 and px[8, 41][1] < 50 and px[8, 41][2] < 50:
+        color = "spade"
+    else:
+        color = "club"
+    return color
+
+def symbol(PILimg):
+    color = differentiate_red_black(PILimg)
+    if color == "red":
+        symbol = differentiate_red(PILimg)
+    elif color == "black":
+        symbol = differentiate_black(PILimg)
+    else:
+        symbol = "unknown"
+    return symbol
+
+for i in im_cartes:
+    print(symbol(i))
+
 
 
 
