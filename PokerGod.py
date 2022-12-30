2eb180b57f88fab0f88630da2e665e66f0999b59
Yutari11 <117772209+Yutari11@users.noreply.github.com>
2022-12-30 17:00:56 +0100

    Add files via upload

--- PokerGod.py
+++ PokerGod.py
@@ -5,13 +5,14 @@ Created on Wed Dec  7 15:48:39 2022
 @author: vlebo
 """
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication,QWidget, QVBoxLayout, QPushButton, QFileDialog , QLabel, QTextEdit
+from PyQt5.QtWidgets import QApplication,QWidget, QVBoxLayout, QPushButton, QFileDialog , QLabel, QLineEdit
 import sys
 from PyQt5.QtGui import QPixmap
 import getCards
 from time import sleep
 import pyautogui
 from PIL.ImageQt import ImageQt
+import treys as t
 
 
 class Window(QWidget):
@@ -23,6 +24,7 @@ class Window(QWidget):
         self.left = 500
         self.width = 400
         self.height = 300
+        self.board = []
 
 
         self.InitWindow()
@@ -52,6 +54,18 @@ class Window(QWidget):
         vbox.addWidget(self.label)
         self.label2 = QLabel("Les cartes sont : ")
         vbox.addWidget(self.label2)
+        
+        self.textbox = QLineEdit(self)
+        self.textbox.setStyleSheet("background-color: rgb(255, 255, 255);")
+        vbox.addWidget(self.textbox)
+        
+        self.btn3 = QPushButton("Show odds")
+        self.btn3.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.btn3.clicked.connect(self.show_odds)
+        vbox.addWidget(self.btn3)
+        
+        self.label3 = QLabel("Score de la main : ")
+        vbox.addWidget(self.label3)
 
 
         self.setLayout(vbox)
@@ -64,8 +78,10 @@ class Window(QWidget):
         pixmap = QPixmap(imagePath)
         self.label.setPixmap(QPixmap(pixmap))
         self.resize(pixmap.width(), pixmap.height())
-        cards = getCards.get_cards_from_screen_link(imagePath)
+        cartes_total = getCards.get_cards_from_screen_link(imagePath)
+        cards = cartes_total[0]
         cards.reverse()
+        self.board = cartes_total[1]
         self.label2.setText("Les cartes sont : " + str(cards))
         self.label2.setFont(QtGui.QFont('Calibri', 30))
     
@@ -77,12 +93,33 @@ class Window(QWidget):
         pixmap = pixmap.scaled(1600,900)
         self.label.setPixmap(QPixmap(pixmap))
         self.resize(pixmap.width(), pixmap.height())
-        cards = getCards.get_cards_from_screen_image(myScreenshot)
+        cartes_total = getCards.get_cards_from_screen_image(myScreenshot)
+        cards = cartes_total[0]
         cards.reverse()
-        self.label2.setText("Les cartes sont : " + str(cards))
+        self.board = cartes_total[1]
+        self.label2.setText("Cartes : " + str(cards))
         self.label2.setFont(QtGui.QFont('Calibri', 30))
-        
-        
+    
+    def show_odds(self):
+        textboxValue = self.textbox.text()
+        liste_hand = textboxValue.split(",")
+        cartes = []
+        for carte in liste_hand:
+            carte_treys = t.Card.new(carte)
+            cartes.append(carte_treys)
+        board_card = [t.Card.new(i) for i in self.board]
+        evaluator = t.Evaluator()
+        evaluation = evaluator.evaluate(board_card, cartes)
+        evaluation = (7462 - evaluation)/7462
+        advice = ""
+        if evaluation < 0.8:
+            advice = "fold"
+        else:
+            advice = "call"
+        self.label3.setText("Score de la main : " + str(evaluation) + ", advice = " + advice)
+        self.label3.setFont(QtGui.QFont('Calibri', 30))
+    
+
 
 
 
